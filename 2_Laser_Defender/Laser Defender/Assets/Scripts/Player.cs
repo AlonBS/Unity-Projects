ef8e462f69cb9f62c622246ecfa4e938a18f6970
Alonbs <alon2351@gmail.com>
2018-12-01 20:52:16 +0200

    Basic functionality of space game
    Signed-off-by: Alonbs <alon2351@gmail.com>

---
+++ 2_Laser_Defender/Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
+
+    private float xMin, xMax, yMin, yMax;
+
+	// Use this for initialization
+	void Start () {
+
+        SetupMoveBoundaries();
+
+       
+
+
+		
+	}
+
+    private IEnumerator PrintShit()
+    {
+        for (;;)
+        {
+            Debug.Log("Hello");
+            yield return new WaitForSeconds(3f);
+           // Debug.Log("World");
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Move();
+        Fire();
+	}
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(Mathf.Clamp(newXPos, xMin, xMax), Mathf.Clamp(newYPos, yMin, yMax));
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+
+        }
+
+
+
+
+    }
+
+
+
+
+    private void SetupMoveBoundaries()
+    {
+        Vector3 mins = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 maxs = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 1));
+
+        xMin = mins.x + padding;
+        yMin = mins.y + padding;
+
+        xMax = maxs.x - padding;
+        yMax = maxs.y - padding;
+
+    }
+}
