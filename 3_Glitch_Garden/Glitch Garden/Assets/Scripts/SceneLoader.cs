7064de0250bb362c12a3d496e9c942ceca2d1350
Alonbs <alon2351@gmail.com>
2018-12-09 17:21:33 +0200

    Added assets and basic UI
    Signed-off-by: Alonbs <alon2351@gmail.com>

---
+++ 3_Glitch_Garden/Glitch Garden/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    [SerializeField] int waitTime = 4;
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+
+        StartCoroutine(LoadStartScene());
+
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private IEnumerator LoadStartScene()
+    {
+
+        yield return new WaitForSeconds(waitTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+	
+}
